6dc4074b4eb0034d178f2652e20ad2dd1e055a32
alexandra348 <mihaelaghica2@gmail.com>
2026-07-20 11:36:56 +0300

    Add: majority-element - submission-1

---
+++ Data Structures & Algorithms/majority-element/submission-1.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int candidate=0;
+        int count=0;
+
+        for(int num : nums)
+        {
+           if(count==0)
+           {
+            candidate=num;
+           }
+
+           if(candidate==num)
+           {
+            count++;
+           }
+           else
+           {
+            count--;
+           }
+        }
+
+        return candidate;
+
+        //scrie in cerinta ca elementul sigur exista, deci algortimul va functioan
+    }
+}
